64a19f2c00f9c5756c82a1424039a287eb87d016
Dmitry-Krtadze <mr.dima.kirtadze@gmail.com>
2025-01-19 19:23:44 +0200

    0.0.1
    -Поворот ника к челу

--- Script/MP/LookCamera.cs
+++ Script/MP/LookCamera.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 
 public class LookCamera : MonoBehaviour
 {
-
     [SerializeField] private Camera myCamera;
+
     private void Awake()
     {
-        myCamera = GetComponentInParent<Camera>();
+        // Если камера не задана в инспекторе, ищем её среди детей PlayerController
+        if (myCamera == null)
+        {
+            myCamera = GetComponentInParent<Camera>();
+        }
+
+        // В качестве альтернативы можно указать главную камеру, если она фиксирована
+        if (myCamera == null)
+        {
+            myCamera = Camera.main;
+        }
     }
 
     private void LateUpdate()
     {
-        transform.LookAt(myCamera.transform.position);
-        transform.Rotate(Vector3.up * 180);
+        if (myCamera != null)
+        {
+            // Ориентируем объект на камеру
+            Vector3 direction = transform.position - myCamera.transform.position;
+            transform.LookAt(transform.position + direction);
+
+            // Если требуется, добавляем поворот на 180 градусов
+            transform.Rotate(Vector3.up * 180);
+        }
     }
 }
