81b2bd160cccacb14ae71708e19c71c70fc8055c
Dmitry-Krtadze <mr.dima.kirtadze@gmail.com>
2025-01-26 19:23:41 +0200

    0.0.1
    -Система оружия хп, выстрелы

---
+++ Script/Items/GunInfo.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "FPS/Add Gun")]
+public class GunInfo : ItemInfo
+{
+    public float Damage;
+}
