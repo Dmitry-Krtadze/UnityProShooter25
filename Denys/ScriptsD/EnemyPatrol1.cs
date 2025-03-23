3ada0f17ee8fd5b532962867a80d6a0e7b66dda8
RadWolf <pgdenyspg@gmail.com>
2025-03-23 20:01:12 +0200

    0.1.1
    Начало переделки врага при помощи NavMEsh

---
+++ Denys/ScriptsD/EnemyPatrol1.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class EnemyPatrol1 : MonoBehaviour
+{
+    // Компонент агента
+    UnityEngine.AI.NavMeshAgent agent;
+    // Массив положений точек назначения
+    public Transform[] goals;
+    // Расстояние на которое необходимо приблизиться к точке
+    public float distanceToChangeGoal;
+    // Номер текущей целевой точки
+    int currentGoal = 0;
+    CharacterController charCtrl;
+    public GameObject player;
+    public float chaseDistance = 5f; // Радиус преследования
+    private NavMeshAgent nMesh; 
+
+    void Start()
+    {
+        // Сохранение компонента агента и направление к первой точке
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = goals[0].position;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        if (player == null) return;
+
+        float playerDistance = Vector3.Distance(transform.position, player.transform.position);
+
+        // Если игрок в радиусе, меняем точку назначения на игрока
+        if (playerDistance < chaseDistance)
+        {
+            agent.destination = player.transform.position;
+            agent.Speed = agent.Speed + 2;
+        }
+        // Если враг не преследует игрока, то патрулирует по точкам
+        else if (agent.remainingDistance < distanceToChangeGoal)
+        {
+            // Смена точки на следующую
+            currentGoal++;
+            if (currentGoal >= goals.Length) currentGoal = 0;
+            agent.destination = goals[currentGoal].position;
+        }
+    }
+}
